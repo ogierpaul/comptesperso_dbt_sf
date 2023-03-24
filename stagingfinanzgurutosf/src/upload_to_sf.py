a3a14406baa79b961b8694c7e1b3fd64f5e4514a
ogierpaul <ogier.paul@gmail.com>
2023-03-24 14:31:30 +0100

    Documentation for the staging process

--- stagingfinanzgurutosf/src/upload_to_sf.py
+++ stagingfinanzgurutosf/src/upload_to_sf.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from stagingfinanzgurutosf.src.read_finanzguru_extract import latest_excel_as_df_prepared
 from upload2sf import upload_df_to_sf, run_single_sql_statement, get_destination, set_env_variables_if_missing, format_triple_to_identifier
 
@@ -16,13 +15,11 @@ def verify_latest_transaction_date(df, stagename:tuple):
 
 
 def upload_latest_excel_to_sf():
-    set_env_variables_if_missing(project_name='comptesperso_dbt_sf', target_name='dev')
+    set_env_variables_if_missing(project_name='comptesperso_dbt_sf', target_name='stg')
     destination = get_destination(table=_table_name)
     df = latest_excel_as_df_prepared()
-    upload_df_to_sf(df, table=_table_name)
+    upload_df_to_sf(df, table=destination[2], overwrite=True)
     verify_latest_transaction_date(df, destination)
 
 
-if __name__ == '__main__':
-    upload_latest_excel_to_sf()
 
---
+++ upload_latest_excel_to_sf_main.sh
@@ -0,0 +1,4 @@
+pyenv local comptesperso_dbt_sf
+#python -m pip install --upgrade -r ./stagingfinanzgurutosf/requirements.txt
+#python -m pip install --upgrade -e ./stagingfinanzgurutosf
+python -m upload_latest_excel_to_sf_main
